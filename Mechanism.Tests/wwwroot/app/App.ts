7073dc0c321948078e6e1a0d090cf2240c02f536
Dia6lo <Dgamjm@gmail.com>
2016-08-31 21:45:08 +1000

    Add initial files.

---
+++ Mechanism.Tests/wwwroot/app/App.ts
@@ -0,0 +1,14 @@
+﻿var renderer: Renderer;
+
+var keyDown = () => {
+    renderer.render();
+    document.body.removeEventListener("keydown", keyDown);
+};
+
+var startup = () => {
+    renderer = new Renderer();
+    document.body.appendChild(renderer.view);
+    document.body.addEventListener("keydown", keyDown);
+}
+
+window.onload = startup;
---
+++ Mechanism/Renderer.ts
@@ -0,0 +1,19 @@
+﻿class Renderer {
+    view: HTMLCanvasElement;
+
+    constructor() {
+        this.view = document.createElement("canvas");
+    }
+
+    private getContext(): CanvasRenderingContext2D {
+        return this.view.getContext("2d");
+    }
+
+    render(): void {
+        const ctx = this.getContext();
+        ctx.fillStyle = "rgb(200,0,0)";
+        ctx.fillRect(10, 10, 50, 50);
+        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+        ctx.fillRect(30, 30, 50, 50);
+    }
+}
