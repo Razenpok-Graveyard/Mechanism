3deb22b24fb82d7d5c2bf27d8b0a274d0b9edeba
Dia6lo <Dgamjm@gmail.com>
2016-09-03 19:45:26 +1000

    Resize editor through css.

--- Workbench/App/Editor.ts
+++ Workbench/App/Editor.ts
@@ -1,23 +1,6 @@
 ﻿class Editor extends Application {
-    private resized = false;
-
     constructor() {
         super();
         this.renderer.backgroundColor = Color.black;
-        this.fitRendererInWindow();
-        window.onresize = () => this.resized = true;
-    }
-
-    render(time: number): void {
-        if (this.resized) {
-            this.fitRendererInWindow();
-            this.resized = false;
-        }
-        super.render(time);
-    }
-
-    private fitRendererInWindow() {
-        this.renderer.width = document.documentElement.clientWidth;
-        this.renderer.height = document.documentElement.clientHeight;
     }
 }
