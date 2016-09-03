69bc0eda23ba7fbe92bb93dbad24084f3b68e492
Dia6lo <Dgamjm@gmail.com>
2016-09-03 18:25:07 +1000

    Add Application.

---
+++ Mechanism/Application.ts
@@ -0,0 +1,31 @@
+﻿class Application {
+    view: HTMLDivElement;
+    renderer: Renderer;
+    root: RenderObject;
+    fps = 0;
+    private time = 0;
+
+    constructor(width: number = 800, height: number = 600) {
+        this.view = document.createElement("div");
+        this.renderer = new Renderer(width, height);
+        this.view.appendChild(this.renderer.view);
+    }
+
+    run(): void {
+        window.requestAnimationFrame((time) => this.firstRender(time));
+    }
+
+    private firstRender(time: number) {
+        this.time = time;
+        this.render(time);
+    }
+
+    render(time: number) {
+        const delta = time - this.time;
+        this.fps = (1 / delta) * 1000;
+        if (this.root)
+            this.renderer.render(this.root);
+        this.time = time;
+        window.requestAnimationFrame((time) => this.render(time));
+    }
+}
