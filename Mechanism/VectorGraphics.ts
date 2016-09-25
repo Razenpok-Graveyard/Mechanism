c0498dc1f0f67ea587dd7fc9ee8fdd96b707b5e0
Dia6lo <Dgamjm@gmail.com>
2016-09-25 21:33:39 +1000

    Enhance vector graphics.

--- Mechanism/VectorGraphics.ts
+++ Mechanism/VectorGraphics.ts
@@ -5,11 +5,41 @@
         this.canvas = canvas;
     }
 
-    drawRect(color: Color, x: number, y: number, width: number, height: number): VectorGraphics {
-        this.canvas.save();
+    fillStyle(color: Color): VectorGraphics {
         this.canvas.fillStyle = color.toCssHex();
+        return this;
+    }
+
+    strokeStyle(lineWidth: number, color?: Color): VectorGraphics {
+        this.canvas.lineWidth = lineWidth;
+        if (color)
+            this.canvas.strokeStyle = color.toCssHex();
+        return this;
+    }
+
+    drawRect(x: number, y: number, width: number, height: number): VectorGraphics {
         this.canvas.fillRect(x, y, width, height);
-        this.canvas.restore();
+        this.canvas.strokeRect(x, y, width, height);
+        return this;
+    }
+
+    drawRoundedRect(x: number, y: number, width: number, height: number, radius: number): VectorGraphics {
+        const canvas = this.canvas;
+        canvas.save();
+        canvas.beginPath();
+        canvas.moveTo(x, y + radius);
+        canvas.lineTo(x, y + height - radius);
+        canvas.quadraticCurveTo(x, y + height, x + radius, y + height);
+        canvas.lineTo(x + width - radius, y + height);
+        canvas.quadraticCurveTo(x + width, y + height, x + width, y + height - radius);
+        canvas.lineTo(x + width, y + radius);
+        canvas.quadraticCurveTo(x + width, y, x + width - radius, y);
+        canvas.lineTo(x + radius, y);
+        canvas.quadraticCurveTo(x, y, x, y + radius);
+        canvas.closePath();
+        canvas.fill();
+        canvas.stroke();
+        canvas.restore();
         return this;
     }
 }
