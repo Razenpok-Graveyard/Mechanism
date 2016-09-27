85e27ae1ed8305a43b3f59b8b9584601f2b5c756
Dia6lo <Dgamjm@gmail.com>
2016-09-27 19:43:09 +1000

    Implement basic input.

--- Mechanism/Application.ts
+++ Mechanism/Application.ts
@@ -3,6 +3,7 @@
     renderer: Renderer;
     root: RenderObject;
     audio: AudioPlayer;
+    input: Input;
     fps = 0;
     private time: number;
 
@@ -12,10 +13,21 @@
         this.view.appendChild(this.renderer.view);
         this.audio = new AudioPlayer();
         this.view.appendChild(this.audio.view);
+        this.input = new Input();
     }
 
     run(): void {
         window.requestAnimationFrame((time) => this.handleAnimationFrame(time));
+        window.onkeydown = event => this.onKeyDown(event);
+        window.onkeyup = event => this.onKeyUp(event);
+    }
+
+    private onKeyDown(event: KeyboardEvent) {
+        this.input.addKeyEvent(this.translateKey(event.code), true);
+    }
+
+    private onKeyUp(event: KeyboardEvent) {
+        this.input.addKeyEvent(this.translateKey(event.code), false);
     }
 
     private handleAnimationFrame(time: number) {
@@ -23,6 +35,7 @@
             this.time = time;
         const delta = (time - this.time) / 1000;
         this.fps = 1 / delta;
+        this.input.processPendingKeyEvents();
         this.update(delta);
         this.render();
         this.time = time;
@@ -38,5 +51,17 @@
     update(delta: number) {
         if (this.root)
             this.root.update(delta);
+    }
+
+    translateKey(code: string) {
+        // TODO
+        switch (code) {
+            case "ArrowUp": return Key.Up;
+            case "ArrowDown": return Key.Down;
+            case "ArrowRight": return Key.Right;
+            case "ArrowLeft": return Key.Left;
+            case "Space": return Key.Space;
+            default: return Key.Unknown;
+        }
     }
 }
---
+++ Mechanism/Input.ts
@@ -0,0 +1,29 @@
+class Input {
+    private previousKeyState: boolean[] = [];
+    private currentKeyState: boolean[] = [];
+    private keyEventQueue: {key: Key, down: boolean}[] = [];
+
+    isKeyPressed(key: Key) {
+        return this.currentKeyState[key];
+    }
+
+    wasKeyPressed(key: Key) {
+        return this.currentKeyState[key] && !this.previousKeyState[key];
+    }
+
+    wasKeyReleased(key: Key) {
+        return !this.currentKeyState[key] && this.previousKeyState[key];
+    }
+
+    addKeyEvent(key: Key, down: boolean) {
+        this.keyEventQueue.push({key: key, down: down});
+    }
+
+    processPendingKeyEvents() {
+        this.previousKeyState = this.currentKeyState.slice();
+        while (this.keyEventQueue.length > 0) {
+            const event = this.keyEventQueue.pop()!;
+            this.currentKeyState[event.key] = event.down;
+        }
+    }
+}
