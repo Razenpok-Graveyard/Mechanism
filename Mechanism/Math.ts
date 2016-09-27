c1aba5e6f3df72f33175ae839cc0da388546e39e
Dia6lo <Dgamjm@gmail.com>
2016-09-27 14:11:36 +1000

    Add half pi.

--- Mechanism/Math.ts
+++ Mechanism/Math.ts
@@ -1,7 +1,9 @@
 ﻿interface Math {
     lerp(amount: number, from: number, to: number): number;
     clamp(value: number, min: number, max: number): number;
+    HALFPI: number;
 }
 
 Math.lerp = (amount, from, to) => from + (to - from) * amount;
 Math.clamp = (value, min, max) => (value < min) ? min : (value > max ? max : value);
+Math.HALFPI = Math.PI / 2;
