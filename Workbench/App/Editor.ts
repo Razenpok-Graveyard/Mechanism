70bea2459394ce1f5510d38fe5d9318cb2b9b133
Andrey Andreev <aandreev@game-forest.com>
2016-11-07 17:29:17 +1000

    Add label experiments.

--- Workbench/App/Editor.ts
+++ Workbench/App/Editor.ts
@@ -5,10 +5,16 @@
         super();
         this.renderer.backgroundColor = Color.black;
         const layout = new StackLayout(Orientation.Horizontal);
+        const propertyView = this.createPropertyView();
+        layout.addChild(propertyView).sizeRequest = 100;
+        const label = new Label("Hello");
+        const widgetView = new WidgetView(label);
+        widgetView.backgroundColor = new Color(0xFFB6C1);
+        layout.addChild(widgetView).sizeRequest = 700;
         const blue = new Rect(Color.blue);
-        const blueWidgetView = new WidgetView(blue);
-        blueWidgetView.backgroundColor = new Color(0xFFB6C1);
-        layout.addChild(blueWidgetView).sizeRequest = 300;
+        blue.position = new Vector2(100, 100);
+        blue.pivot = new Vector2(0, 0);
+        label.addChild(blue);
         const green = new Rect(Color.green);
         green.position = new Vector2(100, 100);
         green.pivot = new Vector2(0, 0);
@@ -16,10 +22,19 @@
         const red = new Rect(Color.red);
         red.position = new Vector2(100, 100);
         green.addChild(red);
-        //const redWidgetView = new WidgetView(red);
-        //layout.addChild(redWidgetView).sizeRequest = 600;
         this.root = layout;
         this.fitRendererInClient();
+    }
+
+    createPropertyView() {
+        const propertyView = new StackLayout(Orientation.Vertical);
+        for (let i = 0; i < 5; i++) {
+            const label = new Label(`Label ${i}`);
+            const widgetView = new WidgetView(label);
+            widgetView.backgroundColor = Color.blue;
+            propertyView.addChild(widgetView).sizeRequest = 50;
+        }
+        return propertyView;
     }
 
     run(): void {
